ce7d92fa6dc3f01de11e354e50cb439574f2e8ac
Leonardo Panatta <leopanatta@gmail.com>
2023-05-25 10:42:06 -0300

    added sensors and callback examples

---
+++ dags/dag_92_sensors/dag_92_1_sensors.py
@@ -0,0 +1,94 @@
+from airflow import DAG
+from airflow.operators.python import BranchPythonOperator
+from airflow.decorators import task, dag
+from airflow.operators.dummy import DummyOperator
+from airflow.sensors.date_time import DateTimeSensor
+
+from datetime import datetime, timedelta
+from typing import Dict
+
+from dag_6_branching.group.process_tasks import process_tasks
+
+import time
+
+partners = {
+    "partner_snowflake": {
+        "name": "snowflake",
+        "path": "/partners/snowflake",
+        "priority": 2,
+        "pool": "snowflake"
+    },
+    "partner_netflix": {
+        "name": "netflix",
+        "path": "/partners/netflix",
+        "priority": 3,
+        "pool": "netflix"
+    },
+    "partner_astronomer": {
+        "name": "astronomer",
+        "path": "/partners/astronomer",
+        "priority": 1,
+        "pool": "astronomer"
+    }
+}
+
+def _choosing_partner_based_on_day(execution_date):
+    day = execution_date.day_of_week
+    if (day == 1):
+        return 'extract_partner_snowflake'
+    if (day == 3):
+        return 'extract_partner_netflix'
+    if (day == 5):
+        return 'extract_partner_astronomer'
+    return 'stop'
+
+default_args = {
+    "start_date": datetime(2023,5,20),
+    "retries":0
+}
+@dag(
+    description="DAG in charge of processing customer data",
+    default_args=default_args,
+    schedule_interval="@daily",
+    dagrun_timeout=timedelta(minutes=10),
+    tags=["data_science", "customers"],
+    catchup=False,
+    max_active_runs=1
+)
+
+def dag_92_1_sensors():
+
+    start = DummyOperator(task_id="start", trigger_rule='all_success', execution_timeout=timedelta(minutes=10))
+
+    delay = DateTimeSensor(
+        task_id='delay',
+        target_time="{{ execution_date.add(hours=9) }}",
+        poke_interval=60 * 60,
+        mode='reschedule',
+        timeout=60 * 60 * 10,
+        #execution_timeout=
+        soft_fail=True,
+        exponential_backoff=True
+    )
+
+    #choosing_partner_based_on_day = BranchPythonOperator(
+    #    task_id="choosing_partner_based_on_day",
+    #    python_callable=_choosing_partner_based_on_day
+    #)
+
+    #stop = DummyOperator(task_id="stop")
+
+    storing = DummyOperator(task_id="storing", trigger_rule='none_failed_or_skipped')
+
+    #choosing_partner_based_on_day >> stop
+    for partner, details in partners.items():
+        @task.python(task_id=f"extract_{partner}", depends_on_past=True, priority_weight=details['priority'], do_xcom_push=False, pool=details['pool'], multiple_outputs=True)
+        def extract(partner_name, partner_path):
+            time.sleep(3)
+            raise ValueError("failed")
+            return {"partner_name": partner_name, "partner_path": partner_path}
+        extracted_values = extract(details['name'], details['path'])
+        start >> extracted_values
+        process_tasks(extracted_values) >> storing
+
+dag = dag_92_1_sensors()
